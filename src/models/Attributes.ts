2af120a7d49bcc243a5e492fbd5279ed1ec5b4d3
Brandon Wie <it.brandonwie@gmail.com>
2020-07-21 00:22:06 +0800

    don't remember now

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -2,12 +2,12 @@ export class Attributes<T> {
 	constructor(private data: T) {}
 
 	get = <K extends keyof T>(key: K): T[K] => {
-		// 'this' will be correctly bound to the instance of attr that we created
+		// 'this' will be correctly bound to the instance of attr that we created BY DECLARING ARROW FUNCTION!!
 		return this.data[key];
 	};
 
 	set(update: T): void {
-		Object.assign(this.data, update);
+		(<any>Object).assign(this.data, update);
 	}
 
 	getAll(): T {
